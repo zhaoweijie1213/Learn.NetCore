f21cde5f5c63f64fffc075809ed6952e5ac88a5a
赵伟杰 <949210784@qq.com>
2020-01-11 15:45:04 +0800

    AliyunTranslation

--- ASP.NET.Core/ASP.NET.Core/Controllers/HomeController.cs
+++ ASP.NET.Core/ASP.NET.Core/Controllers/HomeController.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ASP.NET.Core.Models;
+using HttpOperation.QueryParams;
+using Service.AliyunTranslate;
+using static Service.AliyunTranslate.TranslateHelper;
 
 namespace ASP.NET.Core.Controllers
 {
     public class HomeController : Controller
     {
+        public TranslateHelper translateHelper=new TranslateHelper();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -32,6 +36,27 @@ namespace ASP.NET.Core.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Trasnlate(AliyunTranslateParams translateParams)
+        {
+            var result = "";
+            if (translateParams.TranslationEngine==0)
+            {
+               result = translateHelper.HttpAliyunECommerceTranslate(translateParams.Content,
+               (Language)translateParams.SourceLanguage,
+               (Language)translateParams.TargetLanguage,
+               (Scene)translateParams.Scene, translateParams.FormatType);
+               
+            }
+            else
+            {
+               result = translateHelper.HttpAliyunTranslate(translateParams.Content,
+              (Language)translateParams.SourceLanguage,
+               (Language)translateParams.TargetLanguage,
+               (Scene)translateParams.Scene, translateParams.FormatType);
+            }
+            return new JsonResult(result);
         }
     }
 }
---
+++ ASP.NET.Core/Service/AliyunTranslate/TranslateHelper.cs
@@ -0,0 +1,102 @@
+﻿using Aliyun.Acs.alimt.Model.V20181012;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Exceptions;
+using Aliyun.Acs.Core.Http;
+using Aliyun.Acs.Core.Profile;
+using PublicMethod;
+using System;
+using System.Text;
+using System.Web;
+
+namespace Service.AliyunTranslate
+{
+    public partial class TranslateHelper
+    {
+
+        public string HttpAliyunTranslate(string Content, Language SourceLanguage, Language TargetLanguage, Scene scene, string FormatType = "text")
+        {
+            string res = "";
+            // 构建一个 Client，用于发起请求
+            IClientProfile profile = DefaultProfile.GetProfile(
+                "cn-hangzhou",
+                "LTAI4FjFq4RozfDdzMQACVvY",
+                "WBIDojBKJA0SSqVB5sYjx8VHw9KtoJ");
+            var targetLanguage = TargetLanguage.ToString();
+            if ((int)TargetLanguage == 2)
+            {
+                targetLanguage = "zh-tw";
+            }
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            try
+            {
+                // 构造请求
+                TranslateGeneralRequest request = new TranslateGeneralRequest();
+                request.Method = MethodType.POST; //设置请求
+                request.FormatType = FormatType; //翻译文本的格式  
+                request.Scene = scene.ToString();
+                request.SourceLanguage = SourceLanguage.ToString(); //源语言  
+                request.SourceText = Content; //原文  
+                request.TargetLanguage = targetLanguage; //目标语言  
+                                                         // 发起请求，并得到 Response
+                TranslateGeneralResponse response = client.GetAcsResponse(request);
+                if (response.Code == 200)
+                {
+                    res = response.Data.Translated.ToString();
+                }
+                else
+                {
+                    res = ((ResponseCode)response.Code).ToDescription();
+                }
+            }
+            catch (ServerException ex)
+            {
+                res=ex.ToString();
+            }
+            return res;
+        }
+        /// <summary>
+        /// 电商版本
+        /// </summary>
+        /// <returns></returns>
+        public string HttpAliyunECommerceTranslate(string Content, Language SourceLanguage, Language TargetLanguage, Scene scene,string FormatType ="text")
+        {
+            string res = "";
+            // 构建一个 Client，用于发起请求
+            IClientProfile profile = DefaultProfile.GetProfile(
+                "cn-hangzhou",
+                "LTAI4FjFq4RozfDdzMQACVvY",
+                "WBIDojBKJA0SSqVB5sYjx8VHw9KtoJ");
+            var targetLanguage = TargetLanguage.ToString();
+            if ((int)TargetLanguage == 2)
+            {
+                targetLanguage = "zh-tw";
+            }
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+            try
+            {
+                TranslateECommerceRequest eCommerceRequest = new TranslateECommerceRequest();
+                eCommerceRequest.Scene = scene.ToString();
+                eCommerceRequest.Method = MethodType.POST;// 设置请求方式，POST
+                eCommerceRequest.FormatType = FormatType;  //翻译文本的格式
+                eCommerceRequest.SourceLanguage = SourceLanguage.ToString();  //源语言
+                eCommerceRequest.SourceText = HttpUtility.UrlEncode(Content, Encoding.GetEncoding("utf-8")); //原文
+                eCommerceRequest.TargetLanguage = targetLanguage;  //目标语言
+                TranslateECommerceResponse response = client.GetAcsResponse(eCommerceRequest);
+                if (response.Code==200)
+                {
+                    res = response.Data.Translated.ToString();
+                }
+                else
+                {
+                    res = ((ResponseCode)response.Code).ToDescription();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                res=ex.Message.ToString();
+            }
+            return res;
+        }
+    }
+}
